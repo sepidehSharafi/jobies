43d5f83fcf3192b2614f16233244421a3861b535
Sepideh <sepidehsharafi200@gmail.com>
2024-02-19 00:43:11 +0330

    add AppBar

---
+++ jobies/src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './App.css'
+import AppNavBar from './components/AppNavBar';
+function App() {
+ return(
+    <div>
+         <AppNavBar/>
+    </div>
+ )
+}
+export default App
