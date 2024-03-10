7281ba3750f7189fd8d5b34045a9509121204145
Sepideh <sepidehsharafi200@gmail.com>
2024-03-10 19:43:20 +0330

    display posts in a new link when they are clicked on

--- jobies/src/Pages/Post/DisplayPost.jsx
+++ jobies/src/Pages/Post/DisplayPost.jsx
@@ -1,29 +1,35 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Badge } from '@mui/material';
 import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 import { useState, useEffect } from "react";
-// import { getPost } from '../../httpClient'
 import { useParams, useNavigate } from "react-router-dom";
-import { ThemeProvider } from '@emotion/react';
-import { Container } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
-import { Link } from "react-router-dom";
+import { get } from '../../httpClient'
+import { put } from '../../httpClient'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import CardMedia from '@mui/material/CardMedia';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+
+const DemoPaper = styled(Paper)(({ theme }) => ({
+    width: 120,
+    height: 120,
+    padding: theme.spacing(2),
+    ...theme.typography.body2,
+    textAlign: 'center',
+}));
+
+function SquareCorners() {
+    return (
+        // <Stack direction="row" spacing={2}>
+        //   <DemoPaper square={false}>{post?.posttitle}, {post?.postcontent}</DemoPaper>
+        // </Stack>
+        <div></div>
+    );
+}
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -46,15 +52,17 @@ export default function DisplayPost() {
 
     const [like, setLike] = useState("");
     const [dislike, setDisLike] = useState("");
-    const [post, setPost] = useState("");
 
-    
-    // const fetchPost = async () => {
-    //     const response = await getPost("/DisplayPost", id);
-    //     console.log(response);
-    //     setPost(response)
-    // };
-    // fetchPost();
+    const [post, setPost] = useState();
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const response = await get(`/posts/${id}`);
+            console.log(response);
+            setPost(response);
+        };
+        fetchPost();
+    }, []);
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
@@ -62,84 +70,66 @@ export default function DisplayPost() {
 
     const handleSubmitLike = async (event) => {
         event.preventDefault();
-        const response = await put("/posts", { postID, like, dislike });
+        const response = await put("/post/like/:id", { id });
         console.log(response);
-        navigate("/")
     };
+
     const theme = createTheme();
     theme.spacing(4);
+    console.log(post);
+    console.log(post?.posttitle);
+    console.log(post?.postid);
     return (
-        <di>made it!!!!!</di>
-        // <ThemeProvider theme={theme}>
-        //     <Container>
-        //         <Card sx={{ maxWidth: 345 }}>
-        //             <CardHeader
-        //                 avatar={
-        //                     <Avatar sx={{ bgcolor: red }} aria-label="recipe">
-        //                         R
-        //                     </Avatar>
-        //                 }
-        //                 action={
-        //                     <IconButton aria-label="settings">
-        //                         <MoreVertIcon />
-        //                     </IconButton>
-        //                 }
+        <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        <Stack direction="column" spacing={4}>
+          <DemoPaper
+           sx={{
+            width: 500, 
+            height: 'auto',
+            p: 2, 
+            textAlign: 'center', 
+            display: 'flex',
+            flexDirection: 'column',
+            direction: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          square={false}>
+           <h1>{post?.posttitle} </h1> 
+            <h2>username: {post?.username}</h2>
+            <h3>post content:{post?.postcontent}</h3>
+            <h4>created at:{post?.created_at}  </h4> 
+            </DemoPaper>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+          </Box>
+        </Stack>
 
-        //                 title={post.post.posttitle}
-        //                 subheader={post.post.created_at}
-
-        //             />
-        //             <CardMedia
-        //                 component="img"
-        //                 height="194"
-        //                 image={post.post.image_url}
-        //                 alt={post.post.postcontent}
-        //             />
-        //             <CardContent>
-        //                 <Typography variant="body2" color="text.secondary">
-        //                 </Typography>
-        //             </CardContent>
-        //             <CardActions disableSpacing>
-        //                 <SentimentVerySatisfiedIcon onClick={handleSubmitLike}
-        //                     onChange={(e) => setLike(e.target.value)}
-        //                 >
-        //                     <Badge badgeContent={post.post.likes_count} color="secondary">
-        //                         <FavoriteIcon />
-        //                     </Badge>
-        //                 </SentimentVerySatisfiedIcon>
-        //                 <SentimentVeryDissatisfiedIcon onClick={handleSubmitLike}
-        //                     onChange={(e) => setDisLike(e.target.value)}
-        //                 >
-        //                     <Badge badgeContent={post.post.dislikes_count} color="secondary">
-        //                         <ShareIcon />
-        //                     </Badge>
-        //                 </SentimentVeryDissatisfiedIcon>
-        //                 <a href={`/post?id=${post.post.postid}`}>
-        //                     <ExpandMore
-        //                         expand={expanded}
-        //                         onClick={handleExpandClick}
-        //                         aria-expanded={expanded}
-        //                         aria-label="show more"
-        //                     >
-        //                         <ExpandMoreIcon />
-        //                     </ExpandMore>
-        //                 </a>
-        //             </CardActions>
-        //             <Collapse in={expanded} timeout="auto" unmountOnExit>
-        //                 <CardContent>
-        //                     <Typography paragraph>
-        //                     </Typography>
-        //                     <Typography paragraph>
-        //                     </Typography>
-        //                     <Typography paragraph>
-        //                     </Typography>
-        //                     <Typography>
-        //                     </Typography>
-        //                 </CardContent>
-        //             </Collapse>
-        //         </Card>
-        //     </Container>
-        // </ThemeProvider>
+        <CardMedia
+        width={500}
+        component="img"
+        height="194"
+        image={post?.image_url}
+      />
+      <SentimentVerySatisfiedIcon></SentimentVerySatisfiedIcon>
+      <SentimentVeryDissatisfiedIcon></SentimentVeryDissatisfiedIcon>
+<label>comment</label>
+<TextField placeholder='write a comment'></TextField>
+      </Box>
     );
 }
 
